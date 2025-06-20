00bf24e825abf330e9a725144dfc73b404d8bf53
Thales Manoel <thalesdesenvolvedor@gmail.com>
2025-06-20 20:43:17 -0300

    Modificacao na configuracao de banco de dados para utilizar .env

--- BackEnd/src/config/db.js
+++ BackEnd/src/config/db.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: 'admin123',
-  database: 'meu_sistema',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
